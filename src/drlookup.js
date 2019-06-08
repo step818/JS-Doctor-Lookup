5e30942d8f4003c5bd8c7897e0846591aa95a205
Stephen Trewick <stephent818@gmail.com>
2019-06-08 01:58:48 -0700

    editted UI to be more readable

--- src/drlookup.js
+++ src/drlookup.js
@@ -2,10 +2,11 @@
 
 export class DoctorLookupService {
 
-  getDoctorByCondition(condition) {
+  getDoctorByCondition(name) {
+
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=wa-seattle&skip=0&limit=11&user_key=4df9d59bd9a27c19344ad10d1b395dbb`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${name}&location=wa-seattle&skip=0&limit=11&user_key=4df9d59bd9a27c19344ad10d1b395dbb`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -2,17 +2,27 @@ import { DoctorLookupService } from './drlookup.js';
 import $ from 'jquery';
 
 $(document).ready(function() {
-  $('#find').click(function() {
-    let search = $('#search').val();
-    $('#search').val("");
+  $('#drForm').submit(function(e) {
+    e.preventDefault();
 
+    let name = $('#name').val();
     let doctorSearch = new DoctorLookupService();
-    let promise = doctorSearch.getDoctorByCondition(search);
-
+    let promise = doctorSearch.getDoctorByCondition(name);
     promise.then(function(response) {
+
       let body = JSON.parse(response);
-      $('.showDoctor').text(``);
-      $('.showErrors').text(`There was an error in processing your request. ${error.message}`);
-    });
+
+      if(body.data.length > 0) {
+        for(let i= 0; i<body.data.length; i++) {
+
+          $('.showDoctor').append(`<ul> ${body.data[i].profile.first_name} ${body.data[i].profile.last_name} ${body.data[i].specialties}`);
+        }
+      } else {
+        $('.showDoctor').text('There are no matches for your search input.');
+      }
+    }, function(error) {
+        $('.showErrors').text(`There was an error in processing your request. ${error.message}`);
+      }
+    };
   });
 });
